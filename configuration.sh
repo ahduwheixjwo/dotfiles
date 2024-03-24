0e3e2de73a54cb107cbe418638dfda7681bbc866
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-24 23:07:06 +0800

    Added: Move all file in scripts directory to its location. (This is my personal file. Comment to stop this action...)

--- configuration.sh
+++ configuration.sh
@@ -14,3 +14,14 @@ sleep 1
 cp $PWD/.xinitrc $HOME
 cp -r $PWD/.config/ $HOME/.config/
 
+# This will move all my personal script is scripts direcotry. Comment to stop this action
+# Change directory to scripts
+cd $PWD/scripts/
+
+for file in $(ls); do
+    if [[ "$file" == "30-touchpad.conf" ]]; then
+        sudo cp $file /etc/X11/xorg.conf.d/
+    else
+        sudo cp $file /usr/local/bin/
+    fi
+done
