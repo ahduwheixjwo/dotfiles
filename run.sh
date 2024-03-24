0ffe36d84f9a73605f1b976268e10a300e9999fd
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-24 22:01:21 +0800

    Added if statement to check if git is installed

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 set -e
 printf '\033e'
 
+# Check if git is installed
+if [[ ! -f /usr/bin/git ]]; then
+    sudo pacman -S git
+
 # Check if .cache folder existed
 if [[ ! -d $HOME/.cache ]]; then
     mkdir .cache
@@ -12,6 +16,7 @@ sleep 1
 # Install yay in .cache folder
 cd $HOME/.cache && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
+printf '\033e'
 # Install essential packages
 xPackages="xorg-server xorg-xinit xorg-xrandr xorg-xinput"
 graphicsPackages="mesa lib32-mesa vulkan-intel lib32-vulkan-intel"
@@ -23,7 +28,6 @@ yayPackages="brave-bin picom-git"
 echo "==> Installing essential packages..."
 sleep 1
 sudo pacman -S $xPackages $graphicsPackages $audioPackages $systemPackages $fonts
-
 yay -S $yayPackages
 
 source $PWD/configuration.sh
