ee208df14ef7feee77ef4a0aba8c8c3b5562d2bc
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-24 22:02:17 +0800

    Added command to clone repository and move all files to its location

--- configuration.sh
+++ configuration.sh
@@ -3,3 +3,14 @@
 set -e
 printf '\033e'
 
+# Clone dotfiles repository
+echo "==> Cloning dotfiles repository..."
+sleep 1
+git clone https://github.com/ahduwheixjwo/dotfiles.git && cd dotfiles
+
+printf '\033e'
+echo "==> Moving all files to its location..."
+sleep 1
+cp $PWD/.xinitrc $HOME
+cp -r $PWD/.config/ $HOME/.config/
+
