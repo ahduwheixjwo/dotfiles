69be2fc112f6cf302ab9258b1ab403b0c7deb695
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-25 00:10:28 +0800

    Changed: Personal scripts changed to function 'optional' so that user can choose to continue with that or not

--- configuration.sh
+++ configuration.sh
@@ -14,14 +14,44 @@ sleep 1
 cp $PWD/.xinitrc $HOME
 cp -r $PWD/.config/ $HOME/.config/
 
-# This will move all my personal script is scripts direcotry. Comment to stop this action
-# Change directory to scripts
-cd $PWD/scripts/
 
-for file in $(ls); do
-    if [[ "$file" == "30-touchpad.conf" ]]; then
-        sudo cp $file /etc/X11/xorg.conf.d/
+optional() {
+    # Change directory to scripts
+    cd $PWD/scripts/
+
+    for file in $(ls); do
+        if [[ "$file" == "30-touchpad.conf" ]]; then
+            sudo cp $file /etc/X11/xorg.conf.d/
+        else
+            sudo cp $file /usr/local/bin/
+        fi
+    done
+
+    # Install essential programs ( Bitwarden & Telegram )
+    mkdir $HOME/AppImage
+
+    # Installing Bitwarden & Telegram
+    aria2c -d $HOME/AppImage -o Bitwarden.AppImage "https://vault.bitwarden.com/download/?app=desktop&platform=linux"
+    aria2c -d $HOME/AppImage "https://telegram.org/dl/desktop/linux"
+
+    cd $HOME/AppImage
+
+    chmod +x *.AppImage
+    tar -xvf *.tar.gz
+    rm *.tar.gz
+}
+
+while true; do
+    echo "Do you want to continue with my personal script ? (y/n)
+default=n"
+    read -p ">>" option
+
+    if [[ -z "$option" || "$option" == "n" || "$option" == "N" ]]; then
+        break
+    elif [[ "$option" == "y" || "$option" == "Y" ]]; then
+        optional
     else
-        sudo cp $file /usr/local/bin/
+        echo "Invalid option..."
+        echo ""
     fi
 done
