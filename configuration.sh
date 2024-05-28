9b08ffa1cb4bb24db550fb8dc93207c1fff80564
ahduwheixjwo <rmnaml2468@gmail.com>
2024-05-28 21:49:55 +0800

    Update

--- configuration.sh
+++ configuration.sh
@@ -8,6 +8,8 @@ echo "==> Moving all files to its location..."
 sleep 1
 cp $PWD/.xinitrc $HOME
 cp -r $PWD/.config/* $HOME/.config/
+sudo cp -r $PWD/Fonts/iosevka /usr/share/fonts
+sudo cp -r $PWD/Fonts/JetBrainsMono /usr/share/fonts
 
 optional() {
     # Change directory to scripts
@@ -20,44 +22,6 @@ optional() {
             sudo cp $file /usr/local/bin/
         fi
     done
-
-    # Move fonts
-    cd $HOME/dotfiles/fonts
-
-    sudo cp -r iosevka /usr/share/fonts/
-
-    # Install essential programs ( Bitwarden & Telegram )
-    mkdir $HOME/AppImage
-
-    # Installing Bitwarden & Telegram
-    printf '\033e'
-    echo "==> Installing Bitwarden..."
-    sleep 1
-
-    aria2c -d $HOME/AppImage -o Bitwarden.AppImage "https://vault.bitwarden.com/download/?app=desktop&platform=linux"
-
-    printf '\033e'
-    echo "==> Installing Telegram"
-    sleep 1
-
-    aria2c -d $HOME/AppImage "https://telegram.org/dl/desktop/linux"
-
-    cd $HOME/AppImage
-
-    chmod +x *.AppImage
-    tar -xvf *.tar.xz
-    rm *.tar.xz
-
-    # Configure GKT theme and icon packs
-    mkdir ~/.local/share/themes
-    mkdir ~/.local/share/icons
-
-    cd $HOME/dotfiles/gtk
-
-    # For gtk theme
-    bsdtar -xf Gruvbox-Dark-BL.zip -C $HOME/.local/share/themes
-    # For icon
-    bsdtar -xf gruvbox-plus-icon-pack-5.2.zip -C $HOME/.local/share/themes
 }
 
 while true; do
