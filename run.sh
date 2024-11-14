bff78662fea92a9293e8cd306e065e8c4b6e4dc3
ahduwheixjwo <100501819+ahduwheixjwo@users.noreply.github.com>
2024-11-14 22:31:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,6 @@
 set -e
 printf '\033e'
 
-# Update mirrors. (COMMENT IF YOU DON"T WANT THIS)
-echo "==> Updating pacman mirrorlist..."
-# Check if pacman-contrib is installed...
-if [[ ! -f /usr//bin/rankmirrors ]]; then
-    sudo pacman -S pacman-contrib
-fi
-
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sudo bash -c 'rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist' >/dev/null
-
 # Check if git is installed
 if [[ ! -f /usr/bin/git ]]; then
     sudo pacman -S git
@@ -31,18 +21,21 @@ cd $HOME/.cache && git clone https://aur.archlinux.org/yay.git && cd yay && make
 printf '\033e'
 # Install essential packages
 xPackages="xorg-server xorg-xinit xorg-xrandr xorg-xinput"
-graphicsPackages="libva lib32-libva libva-intel-driver lib32-libva-intel-driver mesa lib32-mesa vulkan-intel lib32-vulkan-intel"
-audioPackages="bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack alsa-utils pavucontrol"
+graphicsPackages="mesa lib32-mesa intel-media-driver vulkan-intel lib32-vulkan-intel"
+audioPackages="lib32-libpulse libpulse pipewire-pulse pipewire-alsa pavucontrol bluez bluez-libs bluez-utils"
 systemPackages="unzip lxappearance acpi thunar firefox usbutils brightnessctl feh i3-gaps i3blocks rofi kitty xwallpaper aria2 android-tools shutter zsh code libinput zip libva-utils"
 fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji"
 yayPackages="picom-git"
-
-sudo systemctl enable bluetooth > /dev/null
-sudo systemctl start bluetooth
 
 echo "==> Installing essential packages..."
 sleep 1
 sudo pacman -S $xPackages $graphicsPackages $audioPackages $systemPackages $fonts
 yay -S $yayPackages
 
+sudo systemctl enable bluetooth > /dev/null
+sudo systemctl start bluetooth
+
+cp $PWD/.xinitrc $HOME
+
 echo "==> Please 'startx' and run ./configuration.sh"
+sleep 2
