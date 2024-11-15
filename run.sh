9394a9490368e9abe988b281fd8f7b52c9b67eac
ahduwheixjwo <rmnaml2468@gmail.com>
2024-11-15 21:54:32 +0800

    Update

--- run.sh
+++ run.sh
@@ -23,14 +23,12 @@ printf '\033e'
 xPackages="xorg-server xorg-xinit xorg-xrandr xorg-xinput"
 graphicsPackages="mesa lib32-mesa intel-media-driver vulkan-intel lib32-vulkan-intel"
 audioPackages="lib32-libpulse libpulse pipewire-pulse pipewire-alsa pavucontrol bluez bluez-libs bluez-utils"
-systemPackages="unzip lxappearance acpi thunar firefox usbutils brightnessctl feh i3-gaps i3blocks rofi kitty xwallpaper aria2 android-tools shutter zsh code libinput zip libva-utils"
+systemPackages="unzip lxappearance acpi thunar firefox usbutils brightnessctl feh i3-gaps i3blocks rofi kitty xwallpaper aria2 android-tools shutter zsh code libinput p7zip libva-utils picom"
 fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji"
-yayPackages="picom-git"
 
 echo "==> Installing essential packages..."
 sleep 1
 sudo pacman -S $xPackages $graphicsPackages $audioPackages $systemPackages $fonts
-yay -S $yayPackages
 
 sudo systemctl enable bluetooth > /dev/null
 sudo systemctl start bluetooth
